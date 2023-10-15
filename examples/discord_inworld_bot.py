5f5b0a8ce676d9cf26fe601b3cd82fba9ffddc9b
atorsvn <122282538+atorsvn@users.noreply.github.com>
2023-10-15 11:57:00 -0400

    Update discord_inworld_bot.py

--- examples/discord_inworld_bot.py
+++ examples/discord_inworld_bot.py
@@ -10,20 +10,13 @@ CONFIG = {
     "bot-command" : ""
 }
 
-# Create a new Intents object with default settings
-# These settings include tracking of messages, reactions, etc.
 intents = discord.Intents.default()
-# Enable tracking of message content
 intents.message_content = True
 
-# Initialize the Bot object with the "!" command prefix
-# and the specified intents
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Create an InWorldChat object with the specified key, secret, and scene
 chat_app = inworld_chat.InWorldChat(CONFIG["inworld-key"], CONFIG["inworld-secret"], CONFIG["inworld-scene"])
 
-# Set up the InWorldChat object
 chat_app.setup()
 
 @bot.command(name=CONFIG["bot-command"])
@@ -34,11 +27,17 @@ async def inworld_command(ctx, *, query):
     :param ctx: context object provided by discord.py, contains message details
     :param query: message content to send as chat
     """
-    # Send a chat message using the InWorldChat object
-    # The message content, author name, channel ID, and author ID are provided as arguments
-    out = chat_app.chat(query, str(ctx.author), str(ctx.channel.id), str(ctx.author.id))
-    # Reply in Discord with the chat output
-    await ctx.reply(out)
+    # Send a typing indicator
+    async with ctx.typing():
+        # Send a chat message using the InWorldChat object
+        out = chat_app.chat(query, str(ctx.author), str(ctx.channel.id), str(ctx.author.id))
+        # (Optional) If needed, you can add an artificial delay here, to simulate
+        # the bot "typing". For example, to wait 2 seconds before replying, you can
+        # uncomment the next line:
+        # await asyncio.sleep(2)
+    
+        # Reply in Discord with the chat output
+        await ctx.reply(out)
 
 @bot.event
 async def on_ready():
@@ -47,5 +46,4 @@ async def on_ready():
     """
     print('Bot is ready')
 
-# Run the bot with the specified token
 bot.run(CONFIG["bot-token"])
